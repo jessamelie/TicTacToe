0041a55d64f101ab7182242d10d848e24e0de23a
jessamelie <jessamelie.sola@gmail.com>
2023-04-18 11:52:58 +0200

    update : mise à jour des cellules + changement de joueur

--- tictactoe.js
+++ tictactoe.js
@@ -3,9 +3,9 @@ const cells = Array.from(document.getElementsByClassName("cell"));
 const gameStatus = document.querySelector("#gameStatus");
 const restartBtn = document.querySelector("#restartButton");
 
-const playerX ="X"
-const playerO = "O"
-let currentPlayer= playerX;
+let X ="X"
+let O = "O"
+let currentPlayer = X
 let gridFilling = Array(9).fill(null)
 
 const winConditions = [                      
@@ -19,19 +19,31 @@ const winConditions = [
     [2,4,6],
 ]
 
-
 //INITIALISATION DU JEU
 const initializedGame = () => {
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));    
+    cells.forEach(cell => cell.addEventListener("click", cellClicked)) 
+}
+function cellClicked(e){
+    
+    const cellId = e.target
+    cellId.innerText = currentPlayer;
+    //recherche d'un index spécifique dans un tableau
+    const index = Array.from(cells).indexOf(cellId);
+    gridFilling[index] = currentPlayer;
+    //changement de joueur
+    currentPlayer = currentPlayer == X ? O : X;
+    gameStatus.textContent = `${currentPlayer}'s turn`;
 }
 
-function cellClicked(e){
-    const cellIndex = e.target.cellIndex
-
-    if(!gridFilling[cellIndex]){
-        gridFilling[cellIndex]=currentPlayer;
-        e.target.innerText = currentPlayer;
-    }
+//POUR RECOMMENCER LA PARTIE 
+restartBtn.addEventListener("click", restartGame)
+function restartGame() {
+    gridFilling.fill(null)
+    cells.forEach(cell => {cell.innerText = ""
+})
+//reinitiation par défaut du joueur
+currentPlayer= X
+gameStatus.textContent = `${currentPlayer}'s turn`
 }
 
 initializedGame()
